24cd178bbfbd5188dad6a81132ce3c931dfbab9f
Colby Cox <coxjc@seas.upenn.edu>
2016-12-08 16:18:24 -0500

    SUBJ: PongTimer MSG: 1) Update various attributes/methods in PongTimer in order to use MS rather than S.

--- PongTimer.java
+++ PongTimer.java
@@ -4,7 +4,7 @@
 public class PongTimer {
     private long startMS = 0L; //Current timestamp in MS
     private long finalMS = 0L; //Final timestamp in MS
-    private long elapsedS = 0L; //SECONDS elapsed since start of timer
+    private long elapsedMS = 0L; //MILLISECONDS elapsed since start of timer
 
     //Instantiates a timer, sets initial MS
     public void startTimer() {
@@ -14,16 +14,16 @@ public class PongTimer {
     //Ends a timer, sets final MS, makes call to set elapsed seconds
     public void endTimer() {
         this.finalMS = System.currentTimeMillis();
-        this.setElapsedS();
+        this.setElapsedMS();
     }
 
-    //Converts difference between final and initial MS into elapsed seconds
-    private void setElapsedS() {
-        this.elapsedS = Math.round((this.finalMS - this.startMS) * .001);
+    //Converts difference between final and initial MS
+    private void setElapsedMS() {
+        this.elapsedMS = this.finalMS - this.startMS;
     }
 
-    //Returns elapsed seconds
-    public long getElapsed() {
-        return this.elapsedS;
+    //Returns elapsed milliseconds
+    public long getElapsedMS() {
+        return this.elapsedMS;
     }
 }
