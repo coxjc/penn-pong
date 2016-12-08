f851c9e3357caf39d4b418decd0cad58fd6662c5
Colby Cox <coxjc@seas.upenn.edu>
2016-12-08 01:11:02 -0500

    SUBJ: HighScoreManager MSG: 1) Add additional qualifications to isValidFormat 2) Add addHighScore, used to add a new score onto a high score file 3) Add getHighScores, used to retrieve a TreeMap of user names and scores of recorded high scores ISSUES/WIP: 1) addHighScore should append, not overwrite file

--- HighScoreManager.java
+++ HighScoreManager.java
@@ -2,6 +2,11 @@
  * Created by coxjc on 12/7/16.
  */
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.util.TreeMap;
+
 /**
  * Scores are stored in a basic .csv file..
  * Scores are stored according to the following format
@@ -14,12 +19,15 @@
 
 public class HighScoreManager {
 
+    //Ensures that a line follows the following format
+    //  "USER'S NAME,NUMERICAL SCORE"
     public static boolean isValidFormat(String line) {
         String scorerName = "";
         String scoreSecs = "";
         try {
-            scorerName = line.substring(0, line.indexOf(','));
-            scoreSecs = line.substring(line.indexOf(','), line.length());
+            scorerName = line.substring(0, line.indexOf(',')).trim();
+            scoreSecs = line.substring(line.indexOf(',') + 1, line.length())
+                    .trim();
         } catch (Throwable e) { //catch array out of bounds and whatnot
             return false;
         }
@@ -27,8 +35,40 @@ public class HighScoreManager {
             return false;
         } else if (!scoreSecs.matches("[0-9]+")) { //regex to match #s only
             return false;
+        } else if (line.indexOf(',') != line.lastIndexOf(',')) {
+            return false; // if there is more than one comma in the line
         }
         return true;
+    }
+
+    public static boolean addHighScore(CSVWriter writer, String line) {
+        if (!isValidFormat(line)) return false;
+        String[] cols = line.split(",");
+        writer.writeNext(cols);
+        try {
+            writer.close();
+            return true;
+        } catch (Throwable e) {
+            return false;
+        }
+    }
+
+    public static TreeMap<Integer, String> getHighScores(CSVReader reader) {
+        String[] nextLine;
+        TreeMap<Integer, String> highScores = new TreeMap<>();
+        try {
+            while ((nextLine = reader.readNext()) != null) {
+                if (isValidFormat(nextLine[0] + "," + nextLine[1])) {
+                    nextLine[0] = nextLine[0].trim();
+                    nextLine[1] = nextLine[1].trim();
+                    highScores.put(Integer.valueOf(nextLine[1]),
+                            nextLine[0]);
+                }
+            }
+        } catch (Throwable e) {
+            throw new IllegalArgumentException();
+        }
+        return highScores;
     }
 
 }
--- HighScoreManagerTest.java
+++ HighScoreManagerTest.java
@@ -1,14 +1,70 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by coxjc on 12/7/16.
  */
 public class HighScoreManagerTest {
+
+    private final String testScores = "/Users/coxjc/Google " +
+            "Drive/Penn/SemI/CIS120/Java/hw09/sampleScores.csv";
+
     @Test
     public void isValidFormatTests() {
-        assertFalse("Empty line returns false",
+        /**
+         * Start failing tests
+         */
+        assertFalse("Null returns false",
                 HighScoreManager.isValidFormat(null));
+        assertFalse("Empty string returns false",
+                HighScoreManager.isValidFormat(""));
+        assertFalse("Correct text without comma returns false",
+                HighScoreManager.isValidFormat("User123"));
+        assertFalse("User without score returns false",
+                HighScoreManager.isValidFormat("User,"));
+        assertFalse("Score without user returns false",
+                HighScoreManager.isValidFormat(",1"));
+        assertFalse("Non-numerical score returns false",
+                HighScoreManager.isValidFormat("User,III"));
+        assertFalse("More than one comma returns false",
+                HighScoreManager.isValidFormat("Us,er,1"));
+        /**
+         * Start passing tests
+         */
+        assertTrue("String user name w/ numerical score returns true",
+                HighScoreManager.isValidFormat("User,1"));
+        assertTrue(HighScoreManager.isValidFormat("AABBCC,112233"));
+        assertTrue("Space around name or score returns true",
+                HighScoreManager.isValidFormat(" User1 , 1 "));
     }
+
+    @Test
+    public void getHighScoresTests() {
+        try {
+            CSVReader reader = new CSVReader(new FileReader(this.testScores));
+            assertEquals(HighScoreManager.getHighScores(reader).size(), 2);
+        } catch (Throwable e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void addHighScoreTests() {
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(this.testScores));
+            assertTrue(HighScoreManager.addHighScore(writer, "USER3, 8"));
+            CSVReader reader = new CSVReader(new FileReader(this.testScores));
+            assertEquals(HighScoreManager.getHighScores(reader).size(), 3);
+        } catch (Throwable e) {
+            fail(e.getMessage());
+        }
+    }
+
+
 }
